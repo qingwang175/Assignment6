344adbc02795dc3961d3bdaf3a8a0d9d85a4d09a
Qing Wang <qingwang@utexas.edu>
2016-04-18 03:15:03 -0500

    Functions on TicketServer
    Simply used synchronized on the MarkSeatAvailable Function, and needs to
    show what we are printing

--- TicketServer.java
+++ TicketServer.java
@@ -19,7 +19,76 @@ public class TicketServer {
 		Thread t = new Thread(serverThread);
 		t.start();
 	}
-}
+	
+	char row = 'A';
+	int seat = 1;
+	
+	//For use on Rows A-X
+	int midRightOffset = 107;
+	int leftOffset = 79;
+	
+	//For use on rows Y-AA
+	//int endRightOffset = 126;
+	//int endLeftOffset = 92;
+	
+	//Spits out the best seat available in a String, in the form "X104" or "AA120"
+	//If no best seat, it spits back a string that tells you so
+	//Had it the way Bates is, but found on Piazza we can simplify to A-Z in the format of Rows C-X, so commented it out (we can always change Back
+	
+	public String bestAvailableSeat () {
+		String temp;
+		
+		if(row > 90 || (row == 90 && seat > 28)) {
+			return "No seats available";
+		} else {
+			temp = Character.toString(row);
+		}
+		
+		//if(row < 'D') {
+		//	temp += (seat + midRightOffset);
+		//} else if (row > 'C') {
+		temp += seatToSeatNum(seat);
+		//} 
+		/* else {
+			if(seat < 8) {
+				temp += (seat + endRightOffset);
+			} else {
+				temp += (seat + endLeftOffset);
+			}
+		}
+		*/
+		return temp;
+	}
+	
+	//Returns a "X102" string 
+	public synchronized String markAvailableSeatTaken () {
+		String temp;
+		if(row == 'Z' && seat == 28) {
+			return null;
+		} else if(seat == 28) {
+			temp =  Integer.toString(row) + seatToSeatNum(seat);
+			seat = 1;
+			row += 1;
+		} else {
+			temp =  Integer.toString(row) + seatToSeatNum(seat);
+			seat += 1;
+		}
+		return temp;
+	}
+	
+	public String printTicketSeat(String seat) {
+		return null;
+	}
+	
+	//Changes from seat 1-28 to the according seat, 101 - 128
+	private int seatToSeatNum (int seat) {
+		if(seat > 21) {
+			return (seat + leftOffset);
+		} else {
+			return (seat + midRightOffset);
+		}
+	}
+} 
 
 class ThreadedTicketServer implements Runnable {
 
