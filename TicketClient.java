4c040f44a9e96749b78fe4dab6de48a7db8dd80d
Qing Wang <qingwang@utexas.edu>
2016-04-18 03:13:27 -0500

    ThreadedTicketClient Name Change

--- TicketClient.java
+++ TicketClient.java
@@ -5,32 +5,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-class ThreadedTicketClient implements Runnable {
-	String hostname = "127.0.0.1";
-	String threadname = "X";
-	TicketClient sc;
-
-	public ThreadedTicketClient(TicketClient sc, String hostname, String threadname) {
-		this.sc = sc;
-		this.hostname = hostname;
-		this.threadname = threadname;
-	}
-
-	public void run() {
-		System.out.flush();
-		try {
-			Socket echoSocket = new Socket(hostname, TicketServer.PORT);
-			// PrintWriter out =
-			new PrintWriter(echoSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-			echoSocket.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-}
-
 public class TicketClient {
 	ThreadedTicketClient tc;
 	String result = "dummy";
@@ -65,3 +39,30 @@ public class TicketClient {
 		}
 	}
 }
+
+class ThreadedTicketClient implements Runnable {
+	String hostname = "127.0.0.1";
+	String threadname = "X";
+	TicketClient sc;
+
+	public ThreadedTicketClient(TicketClient sc, String hostname, String threadname) {
+		this.sc = sc;
+		this.hostname = hostname;
+		this.threadname = threadname;
+	}
+
+	public void run() {
+		System.out.flush();
+		try {
+			Socket echoSocket = new Socket(hostname, TicketServer.PORT);
+			// PrintWriter out =
+			new PrintWriter(echoSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			echoSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
